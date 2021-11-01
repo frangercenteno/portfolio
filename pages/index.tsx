adbd929920c041282a899362774ad0f6bb99a766
Franger Centeno <frangercenteno1@gmail.com>
2021-11-01 17:22:18 -0500

    chore: test2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,53 +2,53 @@ import { FC } from "react";
 
 import Layout from "@/components/Layout";
 import Hero from "@/sections/Hero";
-//import Projects from "@/sections/Projects";
-import Posts from "@/sections/Posts";
+import Projects from "@/sections/Projects";
+//import Posts from "@/sections/Posts";
 
-//import { BASE_API } from "../constants";
+import { BASE_API } from "../constants";
 import { GitHubData } from "@/interfaces/GitHub";
 import Post from "@/interfaces/Post";
-import { getAllPosts } from "@/utils/mdxUtils";
+//import { getAllPosts } from "@/utils/mdxUtils";
 
 interface HomeProps {
   data: GitHubData[];
   posts: Post[];
 }
 
-const Home: FC<HomeProps> = ({ posts }) => {
+const Home: FC<HomeProps> = ({ data }) => {
   return (
     <Layout>
       <Hero />
-      {/* <Projects data={data} /> */}
-      <Posts posts={posts} />
+      <Projects data={data} />
+      {/* <Posts posts={posts} /> */}
     </Layout>
   );
 };
 
 export const getServerSideProps = async () => {
   try {
-    //const res = await fetch(`${BASE_API}repos`);
-    //const data: GitHubData[] = await res.json();
+    const res = await fetch(`${BASE_API}repos`);
+    const data: GitHubData[] = await res.json();
 
-    const posts = await getAllPosts([
-      "slug",
-      "date",
-      "thumbnail",
-      "title",
-      "description",
-      "tags",
-    ]);
+    // const posts = await getAllPosts([
+    //   "slug",
+    //   "date",
+    //   "thumbnail",
+    //   "title",
+    //   "description",
+    //   "tags",
+    // ]);
 
-    // if (!data) {
-    //   return {
-    //     notFound: true,
-    //   };
-    // }
+    if (!data) {
+      return {
+        notFound: true,
+      };
+    }
 
     return {
       props: {
-        // data: data.filter((item) => item.name !== "frangercenteno").slice(0, 4),
-        posts: posts.slice(0, 3),
+        data: data.filter((item) => item.name !== "frangercenteno").slice(0, 4),
+        //posts: posts.slice(0, 3),
       },
     };
   } catch (_) {
