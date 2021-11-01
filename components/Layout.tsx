501afe86ea8434833b1e7929f0643b509465b614
Franger Centeno <frangercenteno1@gmail.com>
2021-11-01 14:56:32 -0500

    chore: seo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,27 +1,18 @@
 import { Fragment, ReactNode, FC } from "react";
-import Head from "next/head";
 
 import NavBar from "@/sections/NavBar";
 import Footer from "@/sections/Footer";
-
+import Meta from "@/components/Meta";
 interface LayoutProps {
   children: ReactNode;
+  pageTitle?: string;
 }
 
-const Layout: FC<LayoutProps> = ({ children }) => {
+const Layout: FC<LayoutProps> = ({ children, pageTitle = "Home" }) => {
   return (
     <Fragment>
-      <Head>
-        <title>Franger Dev</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="Franger Centento Web Developer Portfolio"
-        />
-        <meta name="author" content="Franger Centeno" />
-      </Head>
-      <div className="bg-secondary-default min-h-screen">
+      <Meta pageTitle={pageTitle} />
+      <div className="bg-secondary-default min-h-screen text-secondary-lighter">
         <div className="container">
           <div className="w-full max-w-3xl m-auto">
             <NavBar />
