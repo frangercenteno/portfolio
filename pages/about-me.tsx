077844c7bcfca2ead29ca8aa8ee4812c4934bce1
Franger Centeno <frangercenteno1@gmail.com>
2021-11-01 20:05:59 -0500

    chore: add section skills

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
+
 import Layout from "@/components/Layout";
+import { SKILLS } from "../constants";
 
 const AboutMe = () => {
   return (
@@ -13,25 +16,30 @@ const AboutMe = () => {
         tempore quasi voluptate libero totam saepe commodi hic soluta accusamus
         dolore dignissimos quidem voluptates quas ullam ipsum obcaecati sequi
         porro!
-        <br />
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat
-        tempore quasi voluptate libero totam saepe commodi hic soluta accusamus
-        dolore dignissimos quidem voluptates quas ullam ipsum obcaecati sequi
-        porro!
-        <br />
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat
-        tempore quasi voluptate libero totam saepe commodi hic soluta accusamus
-        dolore dignissimos quidem voluptates quas ullam ipsum obcaecati sequi
-        porro!
-        <br />
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat
-        tempore quasi voluptate libero totam saepe commodi hic soluta accusamus
-        dolore dignissimos quidem voluptates quas ullam ipsum obcaecati sequi
-        porro!
       </p>
+      <div className="py-16">
+        {SKILLS.map((skill) => (
+          <div className="my-4" key={skill.id}>
+            <h2 className="text-2xl font-bold">{skill.title}</h2>
+            <div className="flex items-center flex-wrap mt-5">
+              {skill.skills.map((item, i) => (
+                <div
+                  key={i.toString()}
+                  className="bg-secondary-lighter p-4 flex justify-center items-center relative rounded mr-8 mb-8"
+                >
+                  <Image
+                    src={item.imageUrl}
+                    alt={item.name}
+                    layout="intrinsic"
+                    width={64}
+                    height={64}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </Layout>
   );
 };
